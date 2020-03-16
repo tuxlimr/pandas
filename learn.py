8c07521d5054f2b2c6d09f382557088afc766f21
Ajay Sharma <itsecty.ajay@gmail.com>
2020-03-16 18:36:26 +0530

    Sum and Changing the positin of data

--- learn.py
+++ learn.py
@@ -15,17 +15,20 @@ df = pd.read_csv('sample_data.csv')
 # print(df.tail(3))
 
 # Getting Columns
-print(df.columns)  
+# print(df.columns)  
+
+# find the count of cols
+# print(len(df.columns))
 
 # read particular values from specific column
-print(df['Name'][0:5])  # name is column name 
-print(df[['Name', 'Type 1', 'Type 2']][0:5])  # Additional bracket and more cols 
+# print(df['Name'][0:5])  # name is column name 
+# print(df[['Name', 'Type 1', 'Type 2']][0:5])  # Additional bracket and more cols 
 
 # Rows 
-print(df.iloc[1:4])
+# print(df.iloc[1:4])
 
 # to get the position of particular elemen
-print(df.iloc[2,1]) # 2 is row and 1 is column here 
+# print(df.iloc[2,1]) # 2 is row and 1 is column here 
 
 # iteration of rows and columns 
 
@@ -43,6 +46,30 @@ print(df.iloc[2,1]) # 2 is row and 1 is column here
 
 
 # print(df.sort_values('Name', ascending=False))#ascending and descending
-print(df.sort_values(['HP','Type 2'], ascending=[0,1]))#ascending and descending
+# print(df.sort_values(['HP','Type 2'], ascending=[0,1]))#ascending and descending
         
+# 
+#changes to data 
+df['Total']=  df['HP']+df['Attack']+df['Defense']+df['Sp. Atk']+\
+            df['Sp. Def']+df['Speed']+df['Generation']
+
+
+# print(df.head(5))  
+
+# to drop any particular column 
+df = df.drop(columns=['Speed'])
+# print(df.head(5))  
+
+# to make addition ;
+df['Total'] = df.iloc[:, 4:6].sum(axis=1)# axis=1 for x axis 
+# print(df.head(5))
+
+# Changing the position of column
+# sce ; mv last col postion to fourth place 
+
+cols =  list(df.columns)
+
+df = df[cols[0:4] + [cols[-1]] + cols[5:12]]
+
+print(df.head(5))
 
