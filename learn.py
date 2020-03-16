6cb8246be49baf95d8fff0d4c8b8f45a01c74fb7
Ajay Sharma <itsecty.ajay@gmail.com>
2020-03-16 16:58:51 +0530

    cols, rows, iteration, filter

--- learn.py
+++ learn.py
@@ -10,6 +10,30 @@ df = pd.read_csv('sample_data.csv')
 # df = pd.read_excel('sample.xlsx')
 
 # for top three rows    
-print(df.head(3))
+# print(df.head(3))
 # for last three rows
-print(df.tail(3))
+# print(df.tail(3))
+
+# Getting Columns
+print(df.columns)  
+
+# read particular values from specific column
+print(df['Name'][0:5])  # name is column name 
+print(df[['Name', 'Type 1', 'Type 2']][0:5])  # Additional bracket and more cols 
+
+# Rows 
+print(df.iloc[1:4])
+
+# to get the position of particular elemen
+print(df.iloc[2,1]) # 2 is row and 1 is column here 
+
+# iteration of rows and columns 
+
+# for index, row in df.iterrows():
+    # print(index, row['Name'])
+
+# filter 
+print(df.loc[df['Type 1']=="Grass"]) # Grass is a value which we want to filter from a particular column
+
+
+
