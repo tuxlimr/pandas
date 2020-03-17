9fc7544352d4d5a5cc69601bb7afd20b8851c8db
Ajay Sharma <itsecty.ajay@gmail.com>
2020-03-17 18:37:22 +0530

    mean included

--- learn.py
+++ learn.py
@@ -1,6 +1,7 @@
 import pandas as pd
+import re
 df = pd.read_csv('sample_data.csv')
-
+# print(df.head())
 # for txt file
 # df = pd.read_csv('sample.txt')
 # df = pd.read_csv('sample.txt',delimiter='\t')
@@ -54,10 +55,10 @@ df['Total']=  df['HP']+df['Attack']+df['Defense']+df['Sp. Atk']+\
             df['Sp. Def']+df['Speed']+df['Generation']
 
 
-# print(df.head(5))  
+# print(df.head(100))  
 
 # to drop any particular column 
-df = df.drop(columns=['Speed'])
+# df = df.drop(columns=['Speed'])
 # print(df.head(5))  
 
 # to make addition ;
@@ -69,7 +70,35 @@ df['Total'] = df.iloc[:, 4:6].sum(axis=1)# axis=1 for x axis
 
 cols =  list(df.columns)
 
-df = df[cols[0:4] + [cols[-1]] + cols[5:12]]
+df = df[cols[0:4] + [cols[-1]] + cols[4:12]]
 
-print(df.head(5))
+# print(df.head(5))
 
+
+
+# saving file to diff formats
+# df.to_csv('modified.csv',index=False)
+# df.to_csv('modified.txt',index=False, sep='\t')
+# df.to_excel('modified.xls',index=False)
+
+# filter data Part 2 
+
+# df = df.loc[(df['Type 1']=='Grass') & (df['Type 2'] == "Poison")] # (and Operator)
+# df = df.loc[(df['Type 1']=='Grass') & (df['Type 2'] == "Poison") & (df['HP'] > 70)] # (Or Operator)
+
+# df.to_csv('filtered.csv')
+
+# df = df.loc[df['Name'].str.contains('Mega')] # filter by specific words in names    
+# df = df.loc[~df['Name'].str.contains('Mega')] # Reverse of previous one     
+# df = df.loc[~df['Name'].str.contains('Mega|IVy', flags=re.I, regex=True)] # filter with Regex function Part 1     
+# df = df.loc[df['Name'].str.contains('^pi[a-z]', flags=re.I, regex=True)] # Pi should be at the start of filter with Regex function Part 2    
+
+# df.loc[df['Type 1'] == 'Fire'] = 'Flamer' #( Replace words )
+# df.loc[df['Type 1'] == 'Flamer',['Generation','Legendary']] = 'Test Value' #(Replace words for more than one column)
+# df.loc[df['Total'] > 500,['Generation','Legendary']] = 'Test Value' #(Replace words for more than one column)
+# df.loc[df['Type 1'] == 'Flamer', 'Type 1'] = 'Fire'
+
+
+# Grouping
+df = df.groupby(['Type 1']).mean().sort_values('Defense', ascending=False)  # mean
+print(df.head(30))  
